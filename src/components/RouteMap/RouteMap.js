3f9053559b091e395fdda53eecc84a37b9e5156e
Brian Genisio <BrianGenisio@Gmail.com>
2015-08-06 18:19:41 -0400

    Center the map based on the route stops

--- src/components/RouteMap/RouteMap.js
+++ src/components/RouteMap/RouteMap.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import styles from './RouteMap.css';
 import withStyles from '../../decorators/withStyles';
 
+const LOCATION_MAGNITUDE = 1000000;
+
 @withStyles(styles)
 class RouteDetails extends React.Component {
 
@@ -13,7 +15,7 @@ class RouteDetails extends React.Component {
   drawMap() {
     var mapOptions = {
       center: { lat: 42.2733204, lng: -83.7376894},
-      zoom: 14
+      zoom: 13
     };
 
     let element = ReactDOM.findDOMNode(this.refs.mapcanvas);
@@ -28,13 +30,37 @@ class RouteDetails extends React.Component {
 
     this.markers = this.props.stops.map(s => new window.google.maps.Marker({
       position: {
-        lat: s.lattitude / 1000000,
-        lng: s.longitude / 1000000
+        lat: s.lattitude / LOCATION_MAGNITUDE,
+        lng: s.longitude / LOCATION_MAGNITUDE
       },
       title: s.name
     }));
 
     this.markers.forEach(m => m.setMap(this.map));
+  }
+
+  center(max, min) {
+    return min + (max - min) / 2;
+  }
+
+  getCenter() {
+    let latitudes = _.chain(this.props.stops)
+      .pluck("lattitude")
+      .map(x => parseInt(x));
+
+    let longitudes = _.chain(this.props.stops)
+      .pluck("longitude")
+      .map(x => parseInt(x));
+
+    return {
+      lat: this.center(latitudes.max().value(), latitudes.min().value()) / LOCATION_MAGNITUDE,
+      lng: this.center(longitudes.max().value(), longitudes.min().value()) / LOCATION_MAGNITUDE
+    };
+  }
+
+  setCenter() {
+    if(!this.map) return;
+    this.map.setCenter(this.getCenter());
   }
 
   componentDidMount() {
@@ -44,6 +70,7 @@ class RouteDetails extends React.Component {
   render() {
 
     this.drawMarkers();
+    this.setCenter();
 
     return (
       <div className="RouteMap">
